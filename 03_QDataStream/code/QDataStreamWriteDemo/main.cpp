6825f0552b75b2a0d3da9fff64d804276812585d
czyt1988 <czy.t@163.com>
2016-07-02 14:19:40 +0800

    modify setting
    modify

--- 03_QDataStream/code/QDataStreamWriteDemo/main.cpp
+++ 03_QDataStream/code/QDataStreamWriteDemo/main.cpp
@@ -4,16 +4,37 @@
 #include <QDataStream>
 
 void demo1(QFile& file);
+void demo2(QFile& file);
 const QString FILE_NAME = "czyQDataStreamDemo.file";
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     QFile file(FILE_NAME);
     demo1(file);
+    //demo2(file);
     return a.exec();
 }
 
 void demo1(QFile& file)
+{
+    qDebug()<<__FUNCTION__;
+    if(file.open(QIODevice::WriteOnly))
+    {
+        QDataStream out(&file);
+        char data[256];
+        memset(data,0,256*sizeof(char));
+        char* p = "this is QDataStream demo";
+        strcpy(data,p);
+        out<<(data,256);
+        file.close();
+    }
+    else
+    {
+        qDebug()<<file.errorString();
+    }
+}
+
+void demo2(QFile& file)
 {
     qDebug()<<__FUNCTION__;
     if(file.open(QIODevice::WriteOnly))
